da48fac04c7bad9aecb03d093e0c5c7c098bf4a5
Andrei_Pana <panaandreimihai@gmail.com>
2025-12-25 13:01:32 +0200

    Initial commit: Scaffold Express API

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the POTM API' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
